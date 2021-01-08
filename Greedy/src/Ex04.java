93e106f0bb036331004bf0314d44f5fb919a5dc7
hyunjuuun <hyunjun86120@gmail.com>
2021-01-08 16:08:45 +0900

    그리디 - 1이 될 때까지 (나의 풀이)

---
+++ Greedy/src/Ex04.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Ex04 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		int k = sc.nextInt();
+		int cnt = 0;
+		
+		while(n!=1) {
+			if (n%k == 0) {
+				n /= k;
+			}
+			else {
+				n -= 1;
+			}
+			cnt++;
+		}
+		
+		System.out.println(cnt);
+		
+		sc.close();
+		
+	}
+
+}
