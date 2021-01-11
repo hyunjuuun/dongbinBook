acf82199cc887663d31f5767781d0e0df99653a2
hyunjuuun <hyunjun86120@gmail.com>
2021-01-11 13:43:04 +0900

    alter Q02 solution

--- Greedy/src/Q02.java
+++ Greedy/src/Q02.java
@@ -6,42 +6,22 @@ public class Q02 {
 
 		Scanner sc = new Scanner(System.in);
 		
-		String s = sc.nextLine();
-		int n = s.length();
+		String str = sc.next();
 		
-		int arr[] = new int[n];
+		int result = str.charAt(0) - '0';
 		
-		for(int i=0; i<n; i++) {
-			arr[i] = s.charAt(i) - '0';
-		}
-		
-		int result = 0;
-		
-		int firstIndex=0;
-		int firstNum = 0;
-		
-		for(int i=0; i<n; i++) {
-			if (arr[i] != 0) {
-				firstIndex = i;
-				firstNum = arr[i];
-				break;
-			}
-		}
-		
-		if (firstNum == 0) {
-			System.out.println(0);
-		}
-		else {
-			result = firstNum;
-			for(int i=firstIndex+1; i<n; i++) {
-				if (arr[i] != 0)
-					result *= arr[i];
-			}
+		for(int i=1; i<str.length(); i++) {
+			int num = str.charAt(i) - '0';
+			if (result <= 1 || num <= 1)
+				result += num;
+			else
+				result *= num;
 		}
 		
 		System.out.println(result);
-
+		
 		sc.close();
+		
 
 	}
 
